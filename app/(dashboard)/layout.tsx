d566a66c2aec5fc9bb1033ca90f974a65e565c8c
MHMIZAN09 <221002550@student.green.edu.bd>
2026-07-30 20:31:22 +0600

    dashboard layout and slidebar create

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,24 @@
-const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
-  return <div className="min-h-screen">{children}</div>;
-};
+import { AppSidebar } from '@/components/app-sidebar';
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { getMe } from '../../service/getMe';
 
-export default DashboardLayout;
+export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const result = await getMe();
+  console.log('result', result);
+  return (
+    <TooltipProvider delayDuration={0}>
+      <SidebarProvider>
+        <AppSidebar user={result.data} />
+
+        <SidebarInset>
+          <header className="flex h-16 items-center border-b px-6">
+            <SidebarTrigger />
+          </header>
+
+          <main className="p-6">{children}</main>
+        </SidebarInset>
+      </SidebarProvider>
+    </TooltipProvider>
+  );
+}
